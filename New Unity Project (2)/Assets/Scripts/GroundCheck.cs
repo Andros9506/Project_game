477d893b5d4fd5288c1e475a9faa08f97f4ff8d2
Andrea <halovshalo95@hotmail.it>
2020-10-20 12:15:00 +0200

    no message

---
+++ New Unity Project (2)/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class GroundCheck : MonoBehaviour
+{
+    [SerializeField] private LayerMask platformLayerMask;
+    public bool isGround = true;
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        isGround = collider != null && (((1 << collider.gameObject.layer) & platformLayerMask) != 0);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGround = false;
+    }
+    private void Log()
+    {
+        Debug.Log(isGround);
+    }
+}
---
+++ New Unity Project (2)/Assets/Scripts/Player_Movements.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class Player_Movements : MonoBehaviour
+{
+    // public
+    public float MovementSpeed = 1;
+    public float JumpForce = 1;
+    public Animator animator;
+    [SerializeField] private LayerMask platforms;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    //private 
+    private Rigidbody2D _rigidbody;
+    private float scale;
+    private bool IsJump;
+    private bool IsRunn;
+    private BoxCollider2D boxCollider2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        scale = transform.localScale.x;
+        boxCollider2D = transform.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movement = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(movement));
+
+        if (movement == 0)
+        {
+            animator.SetBool("IsRun", false);
+        }
+        else
+        {
+            animator.SetBool("IsRun", true);
+        }
+        if (movement < 0) // Flip Sprite
+        {
+            transform.localScale = new Vector2(-scale, transform.localScale.y);
+        }
+        if (movement > 0)
+        {
+            transform.localScale = new Vector2(scale, transform.localScale.y);
+        }
+
+        
+        if (IsGround() && (Input.GetButtonDown("Jump")) && (Mathf.Abs(_rigidbody.velocity.y) < 0.001f))
+        {
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+        if (!IsGround())
+           animator.SetBool("IsJump", true);
+        if (IsGround())
+            animator.SetBool("IsJump", false);
+
+
+
+
+    }
+
+        public bool IsGround()
+        {
+        bool b=true;
+            RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, .1f, platforms);
+            //Debug.Log(raycastHit2D.collider);
+        if (raycastHit2D.collider != null)
+        {
+            b = true;
+        }
+        if (raycastHit2D.collider == null)
+        {
+            b = false;
+        }
+        Debug.Log(b);
+        return b;
+        }
+
+
+    
+
+}
